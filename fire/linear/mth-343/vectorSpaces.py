1d5bb677d4fdf63be459815e3be62d767da9525d
nosvagor <cullyn.newman@gmail.com>
2021-10-15 18:29:15 -0700

    2.3 done

--- fire/linear/mth-343/vectorSpaces.py
+++ fire/linear/mth-343/vectorSpaces.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sympy as sp
-import linearFunctions as my
+import linearFunctions as lf
 
 # --- Question 2.1.26 --- #
 print("\n" + "-" * 19)
@@ -23,9 +23,36 @@ B = sp.Matrix(
 )
 AB = A @ B
 
-my.dimCA(A)
-my.dimCA(B)
-my.dimNA(B)
-my.dimCA(AB)
+lf.dimCA(A)
+lf.dimCA(B)
+lf.dimNA(B)
+lf.dimCA(AB)
+print("-" * 19)
 
+# --- Question 2.3.13 --- #
+print("\n" + "-" * 19)
+print("Question 2.3.13:\n")
+
+A = sp.Matrix(
+    [
+        [1, 1, 0],
+        [1, 3, 1],
+        [3, 1, -1],
+    ]
+)
+
+U = sp.Matrix(
+    [
+        [1, 1, 0],
+        [0, 2, 1],
+        [0, 0, 0],
+    ]
+)
+
+lf.dimCA(A)
+lf.dimCA(U)
+lf.dimCAT(A)
+lf.dimCAT(U)
+
+print(np.array(U.rref()[0]), "\n")
 print("-" * 19)
