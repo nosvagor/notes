195ae226fbbb6c2af0aaa9fa2334d876912d281a
nosvagor <cullyn.newman@gmail.com>
2021-10-19 14:23:28 -0700

    calc progress, program 2 done-ish

--- water/cpp/cs-162/prog2/prog2.cpp
+++ water/cpp/cs-162/prog2/prog2.cpp
@@ -32,13 +32,14 @@ void farewell();
 //            |_______|__|_|__|} c++                                         79
 //=============oo--oo==oo--OOO\\===============================================
 int main() {
+  // input array
   char input[INPUT_SIZE];
 
-  // basic input
+  // basic information
   char course[DESG_SIZE];
   char offering[DESG_SIZE];
 
-  // hybrid designators, defaults to in person
+  // hybrid designators
   char lecture[DESG_SIZE];
   char lab[DESG_SIZE];
   char exam[DESG_SIZE];
@@ -46,6 +47,7 @@ int main() {
 
   greeting();
 
+  // get, echo, and confirm user entered information.
   do {
     getClass(input);
     cout << "\nYou entered:\n"
@@ -53,49 +55,48 @@ int main() {
          << endl << endl;
   } while (!confirm());
 
+  // make string easier to work with
   format(input);
+
+  // extract course name and offering type
   extractCourse(input, course, offering);
 
+  // results, if course is online
   if (!strcmp("online", offering)) {
     cout << endl << course
-      << " is offered as an online class! "
+      << " is offered as an "<< offering << "class! "
       << "All content will be be available remotely."
       << endl << endl;
   };
 
+  // results, if course is attend anywhere
   if (!strcmp("aa", offering)) {
     cout << endl << course
-      << " is offered as an attend-anywhere class! "
+      << " is offered as an "<< offering << "class! "
       << "All content is available online,\n"
       << "but any labs, lectures, or exams are offered in person if preferred."
       << endl << endl;
   };
 
+  // extract course content if hybrid, defaults to in person if not specified.
   if (!strcmp("hybrid", offering)) {
-
     decipher(input, lecture, "lect");
     decipher(input, lab, "lab");
     decipher(input, exam, "ex");
     decipher(input, material, "mat");
+
+    // results, if course is hybrid
+    cout << endl << course
+      << " is offered as an "<< offering << "class!\n"
+      << "   - Lectures are " << lecture << ";\n"
+      << "   - labs are " << lab << ";\n"
+      << "   - exams are " << exam << ";\n"
+      << "   - and course materials are " << material << "."
+      << endl << endl;
   }
 
-  cout << lecture << endl;
-  cout << lab << endl;
-  cout << exam << endl;
-  cout << material << endl;
-
-
-  char response {};
-  do {
-    cout << "Would you like to restart? [y/n]: ";
-    cin >> response;
-    cin.ignore(420, '\n');
-  } while (!((response == 'n') || (response == 'y')));
-
-  if (response == 'y') {
-    main();
-  } else farewell();
-
+  // exit, or restart.
+  farewell();
   return 0;
 }
 
@@ -128,6 +129,7 @@ void getClass(char arr[]){
   cin.ignore(420, '\n');
 }
 
+// used to confirm if entered information is correct
 bool confirm() {
   char response {};
 
@@ -141,6 +143,8 @@ bool confirm() {
   return 1;
 }
 
+// turn input to lower case, remove any unusual characters(:-,)
+// that might mess up extraction.
 void format(char input[]) {
   for (int i = 0; i < strlen(input); ++i){
    input[i] = tolower(input[i]);
@@ -150,8 +154,11 @@ void format(char input[]) {
   }
 }
 
+// extract course and offering type.
 void extractCourse(char input[], char course[], char offering[]){
 
+  // extract course, keep track of size in order to add terminating character
+  // for array. Was an issue when restarting program.
   int size {0};
   for (int i = 0; input[i] != ' '; ++i){
     course[i] = toupper(input[i]);
@@ -160,6 +167,8 @@ void extractCourse(char input[], char course[], char offering[]){
   course[size] = '\0';
 
 
+  // overwrite contents of offering with memcpy, if relevant substr is found.
+  // more curse offerings can easily be added.
   if (strstr(input, "hybrid")) {
     memcpy(offering, "hybrid", 7);
   }
@@ -173,14 +182,19 @@ void extractCourse(char input[], char course[], char offering[]){
   }
 }
 
+// get designator, semi-hard coded assuming format like lect-o, lect-r.
+// not sure of a better way, without regex.
+// Fragile function, should be fixed.
 void decipher(char input[], char content[], const char *desg){
-  memcpy(content, "in person", 10);
+  memcpy(content, "in person", 10); // default to in person
 
-  int i = strlen(desg)+1;
+  int i = strlen(desg)+1; // get position of designator
 
+  // get result of strstr, null results keep default assignment.
   const char *result;
   result = strstr(input, desg);
 
+  // non-null results reassign content category.
   if (result != NULL) {
     char des = strstr(input, desg)[i];
     if (des == 'o') memcpy(content, "optional", 9);
@@ -189,6 +203,18 @@ void decipher(char input[], char content[], const char *desg){
 }
 
 void farewell() {
+
+  // ask to restart of exit
+  char response {};
+  do {
+    cout << "Would you like to restart? [y/n]: ";
+    cin >> response;
+    cin.ignore(420, '\n');
+  } while (!((response == 'n') || (response == 'y')));
+
+  if (response == 'y') {
+    main();
+  } else
   cout << endl
        << "╔╗ ┬ ┬┌─┐   ┬ ┬┌─┐┬  ┬┌─┐  ┌─┐  ┌┐ ┌─┐┌─┐┬ ┬┌┬┐┬┌─┐┬ ┬┬    ┌┬┐┬┌┬┐┌─┐┬\n"
        << "╠╩╗└┬┘├┤    ├─┤├─┤└┐┌┘├┤   ├─┤  ├┴┐├┤ ├─┤│ │ │ │├┤ │ ││     │ ││││├┤ │\n"
