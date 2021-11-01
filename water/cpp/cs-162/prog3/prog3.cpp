34161f13adc04abe741cbfc1a5c7ecf6fc4c532a
nosvagor <cullyn.newman@gmail.com>
2021-11-01 13:00:47 -0700

    prog 3 prep

---
+++ water/cpp/cs-162/prog3/prog3.cpp
@@ -0,0 +1,44 @@
+// ┌─┐┬─┐┌─┐┌─┐┬─┐┌─┐┌┬┐  ┌┬┐┬ ┬┬─┐┌─┐┌─┐
+// ├─┘├┬┘│ ││ ┬├┬┘├─┤│││   │ ├─┤├┬┘├┤ ├┤
+// ┴  ┴└─└─┘└─┘┴└─┴ ┴┴ ┴   ┴ ┴ ┴┴└─└─┘└─┘
+// cullyn newman -- cs162
+
+// Meta-purpose: further practice using arrays of characters, but now with the
+// inclusion of structures, data files, and classes.
+
+// Program-purpose: maintain a list of animals that can perform service
+// activities. Data will be organized by breed, species, service,
+// compatibility, and specific (special) information that relates to each
+// animal in question.
+// --
+// This program utilizes an external file called animal.txt that will be read
+// and written to. New input taken from the user will be appended to existing
+// data in file. The user will have the option to supply a different source
+// file.
+
+
+#include <cctype>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+// Constants
+
+// Function Declarations
+
+// Structs
+
+
+
+//             o  o  O  O  0 0
+//            ,______  ____    0
+//            | MAIN \_|[]|_'__Y                PROGRAM 3! STRUCTS!
+//            |_______|__|_|__|} c++                                         79
+//=============oo--oo==oo--OOO\\===============================================
+int main() {
+
+  cout << "hello world" << endl;
+
+  return 0;
+}
