c57523eddbc92c3ad770402b7911f5c5704415c6
nosvagor <nosvagor@gmail.com>
2022-01-14 19:17:48 -0800

    most I can do for today

--- python/mth-371/homework/hw1.py
+++ python/mth-371/homework/hw1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas
+from scipy import stats
 from scipy.linalg import lu, lu_factor, lu_solve
 from scipy.sparse import csc_matrix, random
 from scipy.sparse.linalg import splu, spsolve, spsolve_triangular
@@ -46,7 +47,7 @@ def solveLU(L, U, b, dense=False, sparse=False):
 
         return x
 
-    # Haven't adjusted for sparse, kinda of works still?
+    # Haven't adjusted for sparse, but below is same as dense using slices.
     # Np.sum(slice * x[i+1:n]) is essentially second for loop
     if sparse:
         # forward sub
@@ -69,17 +70,16 @@ def solveLU(L, U, b, dense=False, sparse=False):
 
 print("\n---DENSE---\n")
 
-np.random.seed(1)
+np.random.seed(1123423)
 n = 4
 A = np.random.rand(n, n)
 b = np.random.rand(1, n)[0]
+
 
 print(f"Matrix:\n {A}\n")
 
 p, L, U = lu(A)
 print(f"permutation: \n{p}\n")
-print(f"lower: \n{L}\n")
-
 x1 = solveLU(L, U, b, dense=True)
 x2 = np.linalg.solve(A, b)
 x3 = lu_solve(lu_factor(A), b)  # should be same as np.linalg.solve
@@ -90,26 +90,35 @@ print(f"x_(np): \n{x2}\n")
 print(f"x_(scipy): \n{x3}\n")
 print(f"b_(A@x_custom): \n{A@x1}\n")
 print(f"b_(A@x_np): \n{A@x2}\n")
-print(
-    "difference between output and ground truth is :",
-    np.linalg.norm(b - A @ x2),
-)
 
 print("\n---SPARSE---\n")
 
-n = 25
-A = random(n, n, density=0.25, format="csc")
+n = 5
+rvs = stats.poisson(4, loc=3).rvs
+A = random(n, n, density=0.25, format="csc", data_rvs=rvs, dtype="int")
+
+# make sure it's nonsingular by creating diagonal data entries
+D = np.random.randint(1, 10, (1, n))[0]
+for i in range(n):
+    A[i, i] = D[i]
+
+print("-" * 79)
+print(f"Matrix(dense):\n{A.todense()}\n")
+print(f"Matrix(csr):\n  (row, col)\t(data)\n\t\n{A}")
+print("-" * 79, "\n")
+
 # A = read_data("data/25.txt")
-b = np.random.rand(n, 1)
+b = np.random.randint(1, 10, (n, 1))
 B = splu(A)
+
+print(f"L(dense):\n{B.L.todense()}\n")
+print(f"U(dense):\n{B.U.todense()}\n")
+
 x_c = solveLU(B.L, B.U, b, sparse=True)
 x_s = B.solve(b)
 
 print(f"given b: \n{b.T[0]}\n")
 print(f"x_custom: \n{x_c}\n")
 print(f"x_(scipy.splu): \n{x_s.T[0]}\n")
+print(f"b(A@x_custom): \n{(A @ x_c).T}\n")
 print(f"b(A@x_scipy): \n{(A @ x_s).T[0]}\n")
-print(
-    "difference between output and ground truth is :",
-    np.linalg.norm(b - A @ x_s),
-)
