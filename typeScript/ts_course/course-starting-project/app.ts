1056150cf2ee5970d749a967ca57f14d5d34b3c0
nosvagor <nosvagor@gmail.com>
2022-04-11 19:32:13 -0700

    ts course progress

--- typeScript/ts_course/course-starting-project/app.ts
+++ typeScript/ts_course/course-starting-project/app.ts
@@ -1,9 +1,25 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+type Combinable = number | string;
+type ConversionOpt = 'string' | 'number';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultOpt: ConversionOpt
+) {
+  let result: Combinable;
+  if (
+    (typeof input1 === 'number' && typeof input2 === 'number') ||
+    resultOpt === 'number'
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-const num1 = 5;
-const num2 = 2.8;
-
-const res = add(num1,num2);
-console.log(res);
+console.log(combine('Hello ', 'World', 'string'));
+console.log(combine(30, 26, 'number'));
+console.log(combine(30, 26, 'string'));
+console.log(combine(30, '26', 'number'));
+console.log(combine(30, '26', 'string'));
