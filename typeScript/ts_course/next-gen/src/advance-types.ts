d07bf256f0187d444460c8c8b8b9f5295467bad3
nosvagor <nosvagor@gmail.com>
2022-04-19 15:52:50 -0700

    course progress

---
+++ typeScript/ts_course/next-gen/src/advance-types.ts
@@ -0,0 +1,1 @@
+console.log('------- ADVANCE TYPES ---------');
--- typeScript/ts_course/next-gen/src/interfaces.ts
+++ typeScript/ts_course/next-gen/src/interfaces.ts
@@ -1,9 +1,23 @@
 console.log('------- INTERFACES ---------');
 
-interface Greetable {
-  name: string;
-  // age: number;
+// type AddFn = (a: number, b: number) => number;
 
+// interface AddFn {
+//   (a: number, b: number): number;
+// }
+//
+// let adds: AddFn;
+
+// adds = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
+
+interface Named {
+  readonly name: string;
+  outPutName?: string;
+}
+
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
