e3fd22bc8e5dd5b78578d5e872ffb9bc8c35b4b1
nosvagor <nosvagor@gmail.com>
2022-05-03 18:27:51 -0700

    instant search example done

--- typeScript/react/animalFarm/src/App.jsx
+++ typeScript/react/animalFarm/src/App.jsx
@@ -1,45 +1,64 @@
-import { useState } from 'react';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { search, animals } = useAnimalSearch();
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type='button' onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className='App-link'
-            href='https://vitejs.dev/guide/features.html'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <main className='App'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <h1>Animal Farm</h1>
+      <div>
+        <input
+          type='text'
+          placeholder='Search'
+          onChange={(e) => search(e.target.value)}
+        />
+      </div>
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+
+        {animals.length === 0 && 'No animals found'}
+      </ul>
+    </main>
   );
+}
+
+// Example component
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
+  );
+}
+
+// Example hook
+function useAnimalSearch() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);
+  }, []);
+
+  const search = async (q) => {
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q);
+  };
+
+  return { search, animals };
 }
 
 export default App;
